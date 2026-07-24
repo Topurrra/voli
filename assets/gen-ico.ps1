932ae5db7efd81a121a15173202de1d873852e6c
topurrra <topurianika96@gmail.com>
2026-07-24 16:44:45 +0400

    chore: v0.2.0, site favicon, crates.io publish metadata

---
+++ assets/gen-ico.ps1
@@ -0,0 +1,51 @@
+﻿# One-time .ico generator from logo.png — run once, commit the .ico, delete this script.
+# powershell -NoProfile -ExecutionPolicy Bypass -File assets\gen-ico.ps1
+Add-Type -AssemblyName System.Drawing
+
+$sizes = @(16, 24, 32, 48, 64, 128, 256)
+$srcPath = Join-Path $PSScriptRoot "logo.png"
+$outPath = Join-Path $PSScriptRoot "voli.ico"
+$src = [System.Drawing.Image]::FromFile($srcPath)
+
+$pngs = @()
+foreach ($s in $sizes) {
+    $bmp = New-Object System.Drawing.Bitmap($s, $s)
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+    $g.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
+    $g.CompositingQuality = [System.Drawing.Drawing2D.CompositingQuality]::HighQuality
+    $g.DrawImage($src, 0, 0, $s, $s)
+    $g.Dispose()
+    $ms = New-Object System.IO.MemoryStream
+    $bmp.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png)
+    $pngs += ,($ms.ToArray())
+    $ms.Dispose()
+    $bmp.Dispose()
+}
+$src.Dispose()
+
+$fs = [System.IO.File]::Create($outPath)
+$bw = New-Object System.IO.BinaryWriter($fs)
+$bw.Write([uint16]0)
+$bw.Write([uint16]1)
+$bw.Write([uint16]$sizes.Count)
+$offset = 6 + ($sizes.Count * 16)
+for ($i = 0; $i -lt $sizes.Count; $i++) {
+    $s = $sizes[$i]
+    $bw.Write([byte]$(if ($s -ge 256) { 0 } else { $s }))
+    $bw.Write([byte]$(if ($s -ge 256) { 0 } else { $s }))
+    $bw.Write([byte]0)
+    $bw.Write([byte]0)
+    $bw.Write([uint16]1)
+    $bw.Write([uint16]32)
+    $bw.Write([uint32]$pngs[$i].Length)
+    $bw.Write([uint32]$offset)
+    $offset += $pngs[$i].Length
+}
+for ($i = 0; $i -lt $sizes.Count; $i++) {
+    $bw.Write($pngs[$i])
+}
+$bw.Dispose()
+$fs.Dispose()
+Write-Host "wrote $outPath ($((Get-Item $outPath).Length) bytes, $($sizes.Count) sizes)"
